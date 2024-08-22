4086ffb41e4f93a2c5fbf72dcfc853b240a8f8b8
pipi-shortstocking <jannie435@naver.com>
2024-08-22 18:28:42 +0900

    init

---
+++ src/main/java/com/muji_backend/kw_muji/KwMujiApplication.java
@@ -0,0 +1,13 @@
+package com.muji_backend.kw_muji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwMujiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KwMujiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muji_backend/kw_muji/KwMujiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muji_backend.kw_muji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwMujiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
