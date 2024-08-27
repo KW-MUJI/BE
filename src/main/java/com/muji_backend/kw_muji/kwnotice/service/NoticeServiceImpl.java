25f956afe79306fa770ba47f2a6168476a221bc3
cjh-19 <gns8812@naver.com>
2024-08-27 22:31:54 +0900

    feat: controller에서 사용될 비즈니스 service(#3)

---
+++ src/main/java/com/muji_backend/kw_muji/kwnotice/service/NoticeServiceImpl.java
@@ -0,0 +1,22 @@
+package com.muji_backend.kw_muji.kwnotice.service;
+
+import com.muji_backend.kw_muji.kwnotice.dto.response.NoticeResponse;
+import com.muji_backend.kw_muji.kwnotice.repository.NoticeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NoticeServiceImpl implements NoticeService {
+
+    private final NoticeRepository noticeRepository;
+
+    public NoticeServiceImpl(NoticeRepository noticeRepository) {
+        this.noticeRepository = noticeRepository;
+    }
+
+    @Override
+    public List<NoticeResponse> getKwHomeNotices(int page, String searchVal, String srCategoryId) {
+        return noticeRepository.getKwHomeNotices(page, searchVal, srCategoryId);
+    }
+}
