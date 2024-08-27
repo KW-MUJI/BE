0db5dc30f2ece7b1cba4de290b02832171915d44
cjh-19 <gns8812@naver.com>
2024-08-27 22:45:27 +0900

    feat: controller 구현-임시 api(#3)

---
+++ src/main/java/com/muji_backend/kw_muji/kwnotice/controller/NoticeController.java
@@ -0,0 +1,25 @@
+package com.muji_backend.kw_muji.kwnotice.controller;
+
+import com.muji_backend.kw_muji.kwnotice.dto.response.NoticeResponse;
+import com.muji_backend.kw_muji.kwnotice.service.NoticeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class NoticeController {
+
+    @Autowired
+    NoticeService noticeService;
+
+    @GetMapping("/api/notices")
+    public List<NoticeResponse> getKwHomeNotices(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(required = false) String searchVal,
+            @RequestParam(required = false) String srCategoryId) {
+        return noticeService.getKwHomeNotices(page, searchVal, srCategoryId);
+    }
+}
