f642fbe040e82e38b579cbeb2ad02da38bba8f8a
cjh-19 <gns8812@naver.com>
2024-09-18 04:19:30 +0900

    feat: result service 로직(#36)

--- src/main/java/com/muji_backend/kw_muji/survey/repository/ResponseRepository.java
+++ src/main/java/com/muji_backend/kw_muji/survey/repository/ResponseRepository.java
@@ -4,6 +4,9 @@ import com.muji_backend.kw_muji.common.entity.ResponseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ResponseRepository extends JpaRepository<ResponseEntity, Long> {
+    List<ResponseEntity> findBySurveyId(Long surveyId);
 }
