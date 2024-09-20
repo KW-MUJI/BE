e74f4b47fac2d071505a8fe4887483d617d584b2
pipi-shortstocking <jannie435@naver.com>
2024-09-20 16:15:32 +0900

    feat: 마이페이지 회원정보 수정 진입(#39)

---
+++ src/main/java/com/muji_backend/kw_muji/mypage/controller/MypageController.java
@@ -0,0 +1,42 @@
+package com.muji_backend.kw_muji.mypage.controller;
+
+import com.muji_backend.kw_muji.common.entity.UserEntity;
+import com.muji_backend.kw_muji.mypage.dto.UserInfoDTO;
+import com.muji_backend.kw_muji.mypage.service.MypageService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/mypage")
+public class MypageController {
+    private final MypageService mypageService;
+
+    private final PasswordEncoder pwdEncoder = new BCryptPasswordEncoder();
+
+    @PostMapping("/checkPw")
+    public ResponseEntity<Map<String, Object>> enterUpdate(@AuthenticationPrincipal UserEntity userInfo, @RequestBody UserInfoDTO dto) {
+        try {
+            if(!mypageService.equalPassword(userInfo.getEmail(), dto.getPassword(), pwdEncoder)) {
+                throw new IllegalArgumentException("비밀번호가 일치하지 않음");
+            }
+
+            return ResponseEntity.ok().body(Map.of("code", 200, "data", true));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(Map.of("code", 400, "data", e.getMessage()));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(500).body(Map.of("code", 500, "data", "비밀번호 확인 오류. 잠시 후 다시 시도해주세요."));
+        }
+    }
+}
---
+++ src/main/java/com/muji_backend/kw_muji/mypage/service/MypageService.java
@@ -0,0 +1,22 @@
+package com.muji_backend.kw_muji.mypage.service;
+
+import com.muji_backend.kw_muji.common.entity.UserEntity;
+import com.muji_backend.kw_muji.mypage.repository.MypageRepository;
+import com.muji_backend.kw_muji.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class MypageService {
+    private final UserRepository userRepo;
+
+    public Boolean equalPassword(final String email, final String password, final PasswordEncoder encoder) {
+        final UserEntity user = userRepo.findByEmail(email);
+
+        return user != null && encoder.matches(password, user.getPassword());
+    }
+}
