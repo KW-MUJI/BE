618af469ff75175fca071fbb6cbb0472a82388ce
cjh-19 <gns8812@naver.com>
2024-09-24 16:56:04 +0900

    feat: swagger, security 추가(#42)

--- src/main/java/com/muji_backend/kw_muji/common/config/SwaggerConfig.java
+++ src/main/java/com/muji_backend/kw_muji/common/config/SwaggerConfig.java
@@ -3,22 +3,39 @@ package com.muji_backend.kw_muji.common.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
+        SecurityScheme securityScheme = getSecurityScheme();
+        SecurityRequirement securityRequirement = getSecurityRequireMent();
+
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(List.of(securityRequirement))
                 .info(apiInfo());
     }
 
     private Info apiInfo() {
         return new Info()
-                .title("API Documentation")
-                .description("springdoc Swagger UI 테스트")
+                .title("광운 무인양품 - 광운 대학 생활 도우미")
+                .description("광운 무인양품 BE API Swagger UI")
                 .version("1.0.0");
+    }
+
+    private SecurityScheme getSecurityScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+    }
+
+    private SecurityRequirement getSecurityRequireMent() {
+        return new SecurityRequirement().addList("bearerAuth");
     }
 }
