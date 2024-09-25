80de35207879ede8e9b77260cd4940baa38f8e8d
cjh-19 <gns8812@naver.com>
2024-09-25 16:15:31 +0900

    feat: requestDto 설정(#49)

---
+++ src/main/java/com/muji_backend/kw_muji/calendar/dto/request/CalendarRequestDto.java
@@ -0,0 +1,15 @@
+package com.muji_backend.kw_muji.calendar.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class CalendarRequestDto {
+    private Long userId;
+    private Long projectId;           // 프로젝트 ID (null이면 개인 일정)
+    private String title;
+    private LocalDateTime eventDate;
+}
