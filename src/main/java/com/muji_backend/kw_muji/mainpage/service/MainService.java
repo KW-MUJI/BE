c5bcc4fa6bf453c70b4e1b7824f1534a5265699d
cjh-19 <gns8812@naver.com>
2024-10-02 17:49:47 +0900

    feat: 공지사항 매핑(#75)

--- src/main/java/com/muji_backend/kw_muji/mainpage/service/MainService.java
+++ src/main/java/com/muji_backend/kw_muji/mainpage/service/MainService.java
@@ -1,11 +1,17 @@
 package com.muji_backend.kw_muji.mainpage.service;
 
+import com.muji_backend.kw_muji.calendar.dto.response.CalendarResponseDto;
 import com.muji_backend.kw_muji.calendar.service.CalendarService;
+import com.muji_backend.kw_muji.kwnotice.dto.response.NoticeResponse;
 import com.muji_backend.kw_muji.kwnotice.service.NoticeService;
 import com.muji_backend.kw_muji.mainpage.dto.response.MainResponseDto;
+import com.muji_backend.kw_muji.survey.dto.response.SurveyResponseDto;
 import com.muji_backend.kw_muji.survey.service.SurveyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +22,38 @@ public class MainService {
     private final CalendarService calendarService;
 
     public MainResponseDto getMainInfo(String yearMonth) {
+        // 공지사항 불러오기 및 매핑
+        MainResponseDto.Notices notices = new MainResponseDto.Notices(
+                mapToNoticeItems(noticeService.getKwHomeNotices(1, "", "").getNotices()),
+                mapToNoticeItems(noticeService.getKwHomeNotices(1, "", "0").getNotices()),
+                mapToNoticeItems(noticeService.getKwHomeNotices(1, "", "1").getNotices()),
+                mapToNoticeItems(noticeService.getKwHomeNotices(1, "", "2").getNotices()),
+                mapToNoticeItems(noticeService.getKwHomeNotices(1, "", "4").getNotices())
+        );
 
+        // 설문조사 불러오기 (최대 4개)
+        // List<SurveyResponseDto.SurveyItemDto> surveys = surveyService.getSurveys();
+
+        // 캘린더 이벤트 불러오기
+        // CalendarResponseDto.EventGroup events = calendarService.getCalendarEvents(yearMonth);
+
+        return MainResponseDto.builder()
+                .notices(notices)
+                .surveys(surveys)
+                .events(events)
+                .build();
+    }
+
+    // == Private Methods ==
+
+    // NoticeResponse.Notice 리스트를 MainResponseDto.NoticeItem 리스트로 변환하는 매핑 함수
+    private List<MainResponseDto.NoticeItem> mapToNoticeItems(List<NoticeResponse.Notice> notices) {
+        return notices.stream()
+                .map(notice -> new MainResponseDto.NoticeItem(
+                        notice.getTitle(),
+                        notice.getLink(),
+                        notice.getUpdatedDate()
+                ))
+                .collect(Collectors.toList());
     }
 }
