8dbfb1d4745c82e2fbb702ca21081cebc278b302
cjh-19 <gns8812@naver.com>
2024-10-02 17:24:50 +0900

    feat: 메인페이지 controller(#75)

---
+++ src/main/java/com/muji_backend/kw_muji/mainpage/controller/MainController.java
@@ -0,0 +1,27 @@
+package com.muji_backend.kw_muji.mainpage.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/mainpage")
+public class MainController {
+
+    @GetMapping("/{yearMonth}")
+    public ResponseEntity<?> getMainInfo(@PathVariable("yearMonth") String yearMonth) {
+        try {
+            return ResponseEntity.ok().body(Map.of("code", 200 , "data", response));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(Map.of("code", 400, "message", e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(Map.of("code", 500, "message", e.getMessage()));
+        }
+    }
+}
