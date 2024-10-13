1b28b476475439894f4376cb7f9d6b792b4a92a4
pipi-shortstocking <jannie435@naver.com>
2024-10-13 15:08:47 +0900

    feat: 팀플 스케쥴링 생성(#80)

--- src/main/java/com/muji_backend/kw_muji/team/repository/TeamRepository.java
+++ src/main/java/com/muji_backend/kw_muji/team/repository/TeamRepository.java
@@ -4,8 +4,10 @@ import com.muji_backend.kw_muji.common.entity.ProjectEntity;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TeamRepository extends JpaRepository<ProjectEntity, Long> {
     List<ProjectEntity> findAllByIsOnGoing(boolean isOnGoing, Sort sort);
+    List<ProjectEntity> findAllByIsOnGoingTrueAndDeadlineAtLessThan(LocalDate date);
 }
