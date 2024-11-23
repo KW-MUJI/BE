4476541b6fd0fdd4e1b972f196ecff5de9091388
pipi-shortstocking <jannie435@naver.com>
2024-11-23 14:48:43 +0900

    feat: 토큰 재발급(#149)

--- src/main/java/com/muji_backend/kw_muji/user/controller/UserController.java
+++ src/main/java/com/muji_backend/kw_muji/user/controller/UserController.java
@@ -1,14 +1,20 @@
 package com.muji_backend.kw_muji.user.controller;
 
+import com.muji_backend.kw_muji.common.config.jwt.JwtProperties;
 import com.muji_backend.kw_muji.common.entity.UserEntity;
 import com.muji_backend.kw_muji.common.entity.enums.UserRole;
 import com.muji_backend.kw_muji.common.security.TokenProvider;
 import com.muji_backend.kw_muji.user.dto.request.*;
 import com.muji_backend.kw_muji.user.dto.response.TokenDTO;
+import com.muji_backend.kw_muji.user.dto.response.UserResponseDTO;
 import com.muji_backend.kw_muji.user.service.MailSendService;
 import com.muji_backend.kw_muji.user.service.RedisService;
 import com.muji_backend.kw_muji.user.service.UserService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +28,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,6 +40,7 @@ public class UserController {
     private final UserService userService;
     private final RedisService redisService;
     private final TokenProvider tokenProvider;
+    private final JwtProperties jwtProperties;
 
     private final PasswordEncoder pwEncoder = new BCryptPasswordEncoder();
 
@@ -108,6 +117,7 @@ public class UserController {
             return ResponseEntity.status(500).body(Map.of("code", 500, "data", "인증번호 확인 오류. 잠시 후 다시 시도해주세요."));
         }
     }
+
     @PostMapping("/signIn")
     public ResponseEntity<Map<String, Object>> signIn(@RequestBody @Valid SignInRequestDTO dto, BindingResult bindingResult) {
         try {
@@ -119,8 +129,8 @@ public class UserController {
                 throw new IllegalArgumentException("로그인 실패");
             }
 
-            final String accessToken = tokenProvider.createAccessToken(user);
-            final String refreshToken = tokenProvider.createRefreshToken(user);
+            final String accessToken = tokenProvider.createAccessToken(Optional.of(user));
+            final String refreshToken = tokenProvider.createRefreshToken(Optional.of(user));
 
             final TokenDTO resDTO = TokenDTO.builder()
                     .accessToken(accessToken)
@@ -170,6 +180,62 @@ public class UserController {
             return ResponseEntity.badRequest().body(Map.of("code", 400, "data", e.getMessage()));
         } catch (RuntimeException e) {
             return ResponseEntity.status(500).body(Map.of("code", 500, "data", "비밀번호 재설정 오류. 잠시 후 다시 시도해주세요."));
+        }
+    }
+
+    // accessToken 재발급
+    @PostMapping("/newToken")
+    public ResponseEntity<?> createNewToken(HttpServletRequest request){
+        try {
+            String token = request.getHeader("Authorization").substring(7);
+            log.info("create new accessToken from : {}", token);
+
+            Claims claims = Jwts.parser()
+                    .setSigningKey(jwtProperties.getSecretKey())
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            Long id = Long.parseLong(claims.getSubject());
+
+            Optional<UserEntity> user = userService.getById(id);
+            final UserResponseDTO resUserDTO = UserResponseDTO.builder()
+                    .id(user.get().getId())
+                    .email(user.get().getEmail())
+                    .accessToken(tokenProvider.createAccessToken(user))
+                    .build();
+
+            return ResponseEntity.ok().body(resUserDTO);
+        }catch (Exception e){
+            log.error("/auth/newToken 실행 중 예외 발생", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("newToken fail");
+        }
+    }
+
+    // refreshToken 재발급
+    @PostMapping("/newRefreshToken")
+    public ResponseEntity<?> createNewRefreshToken(HttpServletRequest request){
+        try {
+            String token = request.getHeader("Authorization").substring(7);
+            log.info("create new refresh Token from : {}", token);
+
+            Claims claims = Jwts.parser()
+                    .setSigningKey(jwtProperties.getSecretKey())
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            Long id = Long.parseLong(claims.getSubject());
+
+            Optional<UserEntity> user = userService.getById(id);
+            final UserResponseDTO resUserDTO = UserResponseDTO.builder()
+                    .id(user.get().getId())
+                    .email(user.get().getEmail())
+                    .refreshToken(tokenProvider.createRefreshToken(user))
+                    .build();
+
+            return ResponseEntity.ok().body(resUserDTO);
+        }catch (Exception e){
+            log.error("/auth/newrefreshToken 실행 중 예외 발생", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("newRefreshToken fail");
         }
     }
 }
