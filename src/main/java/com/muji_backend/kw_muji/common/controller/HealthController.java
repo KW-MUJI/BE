d9276bb6ffecb48fd7565cc45e78aa3849ebd411
cjh-19 <gns8812@naver.com>
2024-11-28 16:48:07 +0900

    feat: health 컨트롤러 추가(#211)

---
+++ src/main/java/com/muji_backend/kw_muji/common/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.muji_backend.kw_muji.common.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public ResponseEntity<HttpStatus> healthCheck(){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
